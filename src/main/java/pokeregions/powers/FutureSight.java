534470ab9e6057d3e11168d234fba47982306d5b
Darkglade1 <jeffrey.zhang.ga@gmail.com>
2024-01-15 12:38:32 -0500

    future sight power now pulses when it's about to trigger

--- src/main/java/pokeregions/powers/FutureSight.java
+++ src/main/java/pokeregions/powers/FutureSight.java
@@ -1,5 +1,10 @@
 package pokeregions.powers;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Interpolation;
 import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.NonStackablePower;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
@@ -7,9 +12,12 @@ import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import pokeregions.PokemonRegions;
+import pokeregions.util.TexLoader;
 
+import static pokeregions.PokemonRegions.makeUIPath;
 import static pokeregions.util.Wiz.*;
 
 public class FutureSight extends AbstractUnremovablePower implements NonStackablePower {
@@ -19,10 +27,16 @@ public class FutureSight extends AbstractUnremovablePower implements NonStackabl
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
     private int turns;
 
+    private float flashTimer;
+    private final Color flashColor = new Color(1.0F, 1.0F, 1.0F, 0.0F);
+    private boolean pulse;
+    private final Texture texture;
+
     public FutureSight(AbstractCreature owner, int amount, int turns) {
         super(POWER_ID, NAME, PowerType.BUFF, false, owner, amount);
         this.turns = turns;
         this.loadRegion("mantra");
+        texture = TexLoader.getTexture(makeUIPath("FutureSightFlash.png"));
         updateDescription();
     }
 
@@ -42,9 +56,56 @@ public class FutureSight extends AbstractUnremovablePower implements NonStackabl
     @Override
     public void updateDescription() {
         if (turns == 1) {
+            beginPulse();
             this.description = DESCRIPTIONS[0] + DESCRIPTIONS[1] + DESCRIPTIONS[3] + amount + DESCRIPTIONS[4];
         } else if (turns == 2) {
             this.description = DESCRIPTIONS[0] + DESCRIPTIONS[2] + DESCRIPTIONS[3] + amount + DESCRIPTIONS[4];
         }
+    }
+
+    private void beginPulse() {
+        this.flashTimer = 1.0F;
+        this.pulse = true;
+    }
+
+    @Override
+    public void renderIcons(SpriteBatch sb, float x, float y, Color c) {
+        super.renderIcons(sb, x, y, c);
+        this.renderPulseFlash(sb, x, y);
+    }
+
+    @Override
+    public void update(int slot) {
+        super.update(slot);
+        this.updatePulseFlash();
+    }
+
+    private void updatePulseFlash() {
+        if (this.flashTimer != 0.0F) {
+            this.flashTimer -= Gdx.graphics.getDeltaTime();
+            if (this.flashTimer < 0.0F) {
+                if (this.pulse) {
+                    this.flashTimer = 1.0F;
+                } else {
+                    this.flashTimer = 0.0F;
+                }
+            }
+        }
+
+    }
+
+    private void renderPulseFlash(SpriteBatch sb, float currentX, float currentY) {
+        float tmp = Interpolation.exp10In.apply(0.0F, 4.0F, this.flashTimer / 2.0F);
+        sb.setBlendFunction(770, 1);
+        this.flashColor.a = this.flashTimer * 0.2F;
+        sb.setColor(this.flashColor);
+        float size = 84.0f;
+        float origin = size / 2;
+        float tmpX = currentX - origin;
+
+        sb.draw(this.texture, tmpX, currentY - origin, origin, origin, size, size, Settings.scale + tmp, Settings.scale + tmp, 0.0f, 0, 0, (int)size, (int)size, false, false);
+        sb.draw(this.texture, tmpX, currentY - origin, origin, origin, size, size, Settings.scale + tmp * 0.66F, Settings.scale + tmp * 0.66F, 0.0f, 0, 0, (int)size, (int)size, false, false);
+        sb.draw(this.texture, tmpX, currentY - origin, origin, origin, size, size, Settings.scale + tmp / 3.0F, Settings.scale + tmp / 3.0F, 0.0f, 0, 0, (int)size, (int)size, false, false);
+        sb.setBlendFunction(770, 771);
     }
 }
