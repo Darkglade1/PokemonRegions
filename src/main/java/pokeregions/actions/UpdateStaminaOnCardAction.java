a737387f97c2893df254df09d6be4dbc41c1b7c9
Darkglade1 <jeffrey.zhang.ga@gmail.com>
2024-02-10 14:11:17 -0500

    oops fix stamina bug

--- src/main/java/pokeregions/actions/UpdateStaminaOnCardAction.java
+++ src/main/java/pokeregions/actions/UpdateStaminaOnCardAction.java
@@ -1,23 +1,28 @@
 package pokeregions.actions;
 
-import pokeregions.cards.AbstractAllyPokemonCard;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.core.Settings;
+import pokeregions.monsters.AbstractPokemonAlly;
 
 public class UpdateStaminaOnCardAction extends AbstractGameAction {
-    AbstractAllyPokemonCard allyCard;
+    AbstractPokemonAlly pokemon;
     private final int staminaChange;
 
-    public UpdateStaminaOnCardAction(AbstractAllyPokemonCard allyCard, int staminaChange) {
+    public UpdateStaminaOnCardAction(AbstractPokemonAlly pokemon, int staminaChange) {
         this.actionType = ActionType.DAMAGE;
         this.duration = Settings.ACTION_DUR_FAST;
-        this.allyCard = allyCard;
+        this.pokemon = pokemon;
         this.staminaChange = staminaChange;
     }
 
+    @Override
     public void update() {
-        int newStamina = allyCard.currentStamina + staminaChange;
-        allyCard.updateStamina(newStamina);
+        if (pokemon.noStaminaCostForTurn && staminaChange < 0) {
+            pokemon.noStaminaCostForTurn = false;
+        } else {
+            int newStamina = pokemon.allyCard.currentStamina + staminaChange;
+            pokemon.allyCard.updateStamina(newStamina);
+        }
         this.isDone = true;
     }
 }
