f601b6c3ed2348a1802d3acfc27aadfbc545d89b
Darkglade1 <jeffrey.zhang.ga@gmail.com>
2024-02-11 15:50:03 -0500

    Big thanks gk

--- src/main/java/pokeregions/cards/cardMods/DampMod.java
+++ src/main/java/pokeregions/cards/cardMods/DampMod.java
@@ -2,35 +2,21 @@ package pokeregions.cards.cardMods;
 
 import basemod.abstracts.AbstractCardModifier;
 import basemod.helpers.CardModifierManager;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.core.Settings;
 import pokeregions.PokemonRegions;
-import pokeregions.util.TexLoader;
 
 import static pokeregions.util.Wiz.atb;
 
 public class DampMod extends AbstractCardModifier {
 
     public static final String ID = PokemonRegions.makeID(DampMod.class.getSimpleName());
-
-    public static final String DAMP1 = PokemonRegions.makeUIPath("Damp1.png");
-    private static final Texture DAMP1_TEXTURE = TexLoader.getTexture(DAMP1);
-    private static final TextureRegion DAMP1_TEXTURE_REGION = new TextureRegion(DAMP1_TEXTURE);
-
-    public static final String DAMP2 = PokemonRegions.makeUIPath("Damp2.png");
-    private static final Texture DAMP2_TEXTURE = TexLoader.getTexture(DAMP2);
-    private static final TextureRegion DAMP2_TEXTURE_REGION = new TextureRegion(DAMP2_TEXTURE);
-
     public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
 
     private boolean alreadyRetain = false;
     private boolean alreadyExhaust = false;
-    private int turns = 2;
+    public int turns = 2;
 
     @Override
     public AbstractCardModifier makeCopy() {
@@ -83,14 +69,5 @@ public class DampMod extends AbstractCardModifier {
     @Override
     public String modifyDescription(String rawDescription, AbstractCard card) {
         return TEXT[0] + rawDescription;
-    }
-
-    @Override
-    public void onRender(AbstractCard card, SpriteBatch sb) {
-        if (turns == 1) {
-            sb.draw(DAMP2_TEXTURE_REGION, card.hb.cX - (float) DAMP2_TEXTURE_REGION.getRegionWidth() / 2, card.hb.cY - (float) DAMP2_TEXTURE_REGION.getRegionHeight() / 2 + (75.0f * card.drawScale * Settings.scale), (float) DAMP2_TEXTURE_REGION.getRegionWidth() / 2, (float) DAMP2_TEXTURE_REGION.getRegionHeight() / 2, DAMP2_TEXTURE_REGION.getRegionWidth(), DAMP2_TEXTURE_REGION.getRegionHeight(), Settings.scale * card.drawScale, Settings.scale * card.drawScale, 0.0f);
-        } else {
-            sb.draw(DAMP1_TEXTURE_REGION, card.hb.cX - (float) DAMP1_TEXTURE_REGION.getRegionWidth() / 2, card.hb.cY - (float) DAMP1_TEXTURE_REGION.getRegionHeight() / 2 + (75.0f * card.drawScale * Settings.scale), (float) DAMP1_TEXTURE_REGION.getRegionWidth() / 2, (float) DAMP1_TEXTURE_REGION.getRegionHeight() / 2, DAMP1_TEXTURE_REGION.getRegionWidth(), DAMP1_TEXTURE_REGION.getRegionHeight(), Settings.scale * card.drawScale, Settings.scale * card.drawScale, 0.0f);
-        }
     }
 }
--- src/main/java/pokeregions/cards/cardMods/WaterLoggedMod.java
+++ src/main/java/pokeregions/cards/cardMods/WaterLoggedMod.java
@@ -2,26 +2,16 @@ package pokeregions.cards.cardMods;
 
 import basemod.abstracts.AbstractCardModifier;
 import basemod.helpers.CardModifierManager;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.core.Settings;
 import pokeregions.PokemonRegions;
-import pokeregions.util.TexLoader;
 
 import static pokeregions.util.Wiz.atb;
 
 public class WaterLoggedMod extends AbstractCardModifier {
 
     public static final String ID = PokemonRegions.makeID(WaterLoggedMod.class.getSimpleName());
-
-    public static final String WATER_LOGGED = PokemonRegions.makeUIPath("WaterLogged.png");
-    private static final Texture WATER_LOGGED_TEXTURE = TexLoader.getTexture(WATER_LOGGED);
-    private static final TextureRegion WATER_LOGGED_TEXTURE_REGION = new TextureRegion(WATER_LOGGED_TEXTURE);
-
     public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
 
     private boolean alreadyRetain = false;
@@ -72,10 +62,5 @@ public class WaterLoggedMod extends AbstractCardModifier {
     @Override
     public String modifyDescription(String rawDescription, AbstractCard card) {
         return TEXT[0] + rawDescription;
-    }
-
-    @Override
-    public void onRender(AbstractCard card, SpriteBatch sb) {
-        sb.draw(WATER_LOGGED_TEXTURE_REGION, card.hb.cX - (float) WATER_LOGGED_TEXTURE_REGION.getRegionWidth() / 2, card.hb.cY - (float) WATER_LOGGED_TEXTURE_REGION.getRegionHeight() / 2 + (75.0f * card.drawScale * Settings.scale), (float) WATER_LOGGED_TEXTURE_REGION.getRegionWidth() / 2, (float) WATER_LOGGED_TEXTURE_REGION.getRegionHeight() / 2, WATER_LOGGED_TEXTURE_REGION.getRegionWidth(), WATER_LOGGED_TEXTURE_REGION.getRegionHeight(), Settings.scale * card.drawScale, Settings.scale * card.drawScale, 0.0f);
     }
 }
