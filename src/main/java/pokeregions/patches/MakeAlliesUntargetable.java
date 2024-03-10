5303f17028c0e938cbb85d5bd6620190639a9e14
Darkglade1 <jeffrey.zhang.ga@gmail.com>
2024-03-10 14:51:50 -0400

    I must target the enemy with taunt

--- src/main/java/pokeregions/patches/MakeAlliesUntargetable.java
+++ src/main/java/pokeregions/patches/MakeAlliesUntargetable.java
@@ -1,13 +1,20 @@
 package pokeregions.patches;
 
 import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.megacrit.cardcrawl.actions.animations.TalkAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.helpers.input.InputHelper;
+import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.monsters.MonsterGroup;
 import javassist.CtBehavior;
 import pokeregions.monsters.AbstractPokemonAlly;
 import pokeregions.powers.Taunt;
 import pokeregions.util.Wiz;
+
+import static pokeregions.PokemonRegions.makeID;
+import static pokeregions.util.Wiz.atb;
 
 @SpirePatch(
         clz = AbstractPlayer.class,
@@ -16,6 +23,9 @@ import pokeregions.util.Wiz;
 )
 // A patch to make allies untargetable by the player
 public class MakeAlliesUntargetable {
+
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString(makeID("TauntNotice"));
+    private static final String[] TEXT = uiStrings.TEXT;
     @SpireInsertPatch(locator = Locator.class, localvars = {"hoveredMonster"})
     public static void MakeHoveredMonsterNull(AbstractPlayer instance, @ByRef AbstractMonster[] hoveredMonster) {
         if (hoveredMonster[0] instanceof AbstractPokemonAlly) {
@@ -28,7 +38,10 @@ public class MakeAlliesUntargetable {
                 break;
             }
         }
-        if (tauntingMonster != null && hoveredMonster[0] != tauntingMonster) {
+        if (tauntingMonster != null && hoveredMonster[0] != null && hoveredMonster[0] != tauntingMonster) {
+            if (InputHelper.justClickedLeft) {
+                atb(new TalkAction(true, TEXT[0], 1.0F, 1.0F));
+            }
             hoveredMonster[0] = null;
         }
     }
