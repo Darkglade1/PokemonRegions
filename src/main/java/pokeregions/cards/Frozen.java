f3d07695a5e77ce41f8fe826b02061a29602a9bb
Darkglade1 <jeffrey.zhang.ga@gmail.com>
2024-11-20 12:35:14 -0500

    rework frozen status

--- src/main/java/pokeregions/cards/Frozen.java
+++ src/main/java/pokeregions/cards/Frozen.java
@@ -1,26 +1,32 @@
 package pokeregions.cards;
 
-import com.megacrit.cardcrawl.actions.common.ExhaustAction;
+import com.megacrit.cardcrawl.actions.common.ReduceCostAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import pokeregions.PokemonRegions;
+
+import static pokeregions.util.Wiz.atb;
 
 public class Frozen extends AbstractEasyCard {
 
     public static final String ID = PokemonRegions.makeID(Frozen.class.getSimpleName());
 
     public Frozen() {
-        super(ID, 1, CardType.STATUS, CardRarity.COMMON, CardTarget.NONE, CardColor.COLORLESS);
+        super(ID, 2, CardType.STATUS, CardRarity.COMMON, CardTarget.NONE, CardColor.COLORLESS);
         exhaust = true;
         setMagic(1);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new ExhaustAction(magicNumber, false));
     }
 
     @Override
     public void upgrade() {
+    }
+
+    @Override
+    public void triggerWhenDrawn() {
+        atb(new ReduceCostAction(this.uuid, this.magicNumber));
     }
 }
