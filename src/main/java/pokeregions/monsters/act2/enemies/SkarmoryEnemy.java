ef5ed07314a06aa6cbcb14a502cd6da8ec28cea6
Darkglade1 <jeffrey.zhang.ga@gmail.com>
2024-11-23 15:12:46 -0500

    rework skarmory enemy

--- src/main/java/pokeregions/monsters/act2/enemies/SkarmoryEnemy.java
+++ src/main/java/pokeregions/monsters/act2/enemies/SkarmoryEnemy.java
@@ -1,26 +1,34 @@
 package pokeregions.monsters.act2.enemies;
 
 import basemod.ReflectionHacks;
+import basemod.helpers.VfxBuilder;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.MathUtils;
 import com.brashmonkey.spriter.Player;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.RollMoveAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.status.Wound;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.MonsterStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.monsters.EnemyMoveInfo;
-import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.powers.MetallicizePower;
+import com.megacrit.cardcrawl.powers.ThornsPower;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
 import pokeregions.BetterSpriterAnimation;
 import pokeregions.PokemonRegions;
 import pokeregions.cards.pokemonAllyCards.act2.Skarmory;
 import pokeregions.monsters.AbstractPokemonMonster;
-import pokeregions.powers.StrongArmor;
 import pokeregions.util.Details;
+import pokeregions.util.ProAudio;
+import pokeregions.util.TexLoader;
 
 import java.util.ArrayList;
+import java.util.function.BiFunction;
 
 import static pokeregions.PokemonRegions.*;
 import static pokeregions.util.Wiz.*;
@@ -38,8 +46,8 @@ public class SkarmoryEnemy extends AbstractPokemonMonster
 
     public final int STATUS = calcAscensionSpecial(2);
     public final int BUFF = calcAscensionSpecial(2);
+    public final int METALLICIZE = 12;
     public final int BLOCK = 14;
-    public final int POWER_AMT = 50;
 
     public SkarmoryEnemy() {
         this(0.0f, 0.0f);
@@ -51,10 +59,10 @@ public class SkarmoryEnemy extends AbstractPokemonMonster
         ((BetterSpriterAnimation)this.animation).myPlayer.speed = 10;
         ((BetterSpriterAnimation)this.animation).myPlayer.setScale(Settings.scale * 1.2f);
         this.type = EnemyType.NORMAL;
-        setHp(calcAscensionTankiness(98), calcAscensionTankiness(110));
+        setHp(calcAscensionTankiness(93), calcAscensionTankiness(105));
         addMove(SPIKES, Intent.DEBUFF);
-        addMove(METAL_CLAW, Intent.ATTACK_DEFEND, calcAscensionDamage(15));
-        addMove(BRAVE_BIRD, Intent.ATTACK, calcAscensionDamage(6), 3);
+        addMove(METAL_CLAW, Intent.ATTACK_DEFEND, calcAscensionDamage(16));
+        addMove(BRAVE_BIRD, Intent.ATTACK, calcAscensionDamage(7), 3);
 
         Player.PlayerListener listener = new PokemonListener(this);
         ((BetterSpriterAnimation)this.animation).myPlayer.addListener(listener);
@@ -63,7 +71,8 @@ public class SkarmoryEnemy extends AbstractPokemonMonster
     @Override
     public void usePreBattleAction() {
         super.usePreBattleAction();
-        applyToTarget(this, this, new StrongArmor(this, POWER_AMT));
+        block(this, METALLICIZE);
+        applyToTarget(this, this, new MetallicizePower(this, METALLICIZE));
     }
 
     @Override
@@ -75,8 +84,45 @@ public class SkarmoryEnemy extends AbstractPokemonMonster
 
         switch (this.nextMove) {
             case SPIKES: {
+                useFastAttackAnimation();
+                Texture spike = TexLoader.getTexture(makeVfxPath("Spike.png"));
+                float spikeDuration = 1.2f;
+                float vfxInternal = 0.3f;
+                int numSpikes = 5;
+                float totalDuration = spikeDuration + (vfxInternal * numSpikes);
+                VfxBuilder builder = new VfxBuilder(spike, this.hb.cX, this.hb.cY, spikeDuration)
+                        .arc(this.hb.cX, this.hb.cY, adp().hb.cX - (100.0f * Settings.scale), AbstractDungeon.floorY, 700.0f * Settings.scale)
+                        .rotateTo(0, 270, VfxBuilder.Interpolations.LINEAR)
+                        .triggerVfxAt(spikeDuration, 1, new BiFunction<Float, Float, AbstractGameEffect>() {
+                            @Override
+                            public AbstractGameEffect apply(Float aFloat, Float aFloat2) {
+                                playAudio(ProAudio.ROCK_THUD);
+                                return new VfxBuilder(spike, adp().hb.cX - (100.0f * Settings.scale), AbstractDungeon.floorY, spikeDuration).setAngle(270).build();
+                            }
+                        });
+                for (int i = 0; i < numSpikes; i++) {
+                    float randX = adp().hb.cX + (MathUtils.random(-250, 250) * Settings.scale);
+                    builder.triggerVfxAt(vfxInternal * (i + 1), 1, new BiFunction<Float, Float, AbstractGameEffect>() {
+                        @Override
+                        public AbstractGameEffect apply(Float aFloat, Float aFloat2) {
+
+                            return new VfxBuilder(spike, SkarmoryEnemy.this.hb.cX, SkarmoryEnemy.this.hb.cY, spikeDuration)
+                                    .arc(SkarmoryEnemy.this.hb.cX, SkarmoryEnemy.this.hb.cY, randX, AbstractDungeon.floorY, 700.0f * Settings.scale)
+                                    .rotateTo(0, 270, VfxBuilder.Interpolations.LINEAR)
+                                    .triggerVfxAt(spikeDuration, 1, new BiFunction<Float, Float, AbstractGameEffect>() {
+                                        @Override
+                                        public AbstractGameEffect apply(Float aFloat, Float aFloat2) {
+                                            playAudio(ProAudio.ROCK_THUD);
+                                            return new VfxBuilder(spike, randX, AbstractDungeon.floorY, spikeDuration).setAngle(270).build();
+                                        }
+                                    }).build();
+                        }
+                    });
+                }
+
+                atb(new VFXAction(builder.build(), totalDuration));
                 intoDiscardMo(new Wound(), STATUS);
-                applyToTarget(this, this, new StrengthPower(this, BUFF));
+                applyToTarget(this, this, new ThornsPower(this, BUFF));
                 break;
             }
             case METAL_CLAW: {
@@ -99,18 +145,14 @@ public class SkarmoryEnemy extends AbstractPokemonMonster
                 break;
             }
         }
-        if (this.hasPower(StrongArmor.POWER_ID)) {
-            makePowerRemovable(this.getPower(StrongArmor.POWER_ID));
-            atb(new RemoveSpecificPowerAction(this, this, StrongArmor.POWER_ID));
-        } else {
-            applyToTarget(this, this, new StrongArmor(this, POWER_AMT));
-        }
         atb(new RollMoveAction(this));
     }
 
     @Override
     protected void getMove(final int num) {
-        if (lastMove(BRAVE_BIRD)) {
+        if (firstMove && AbstractDungeon.ascensionLevel >= 17) {
+            setMoveShortcut(SPIKES, MOVES[SPIKES]);
+        } else if (lastMove(BRAVE_BIRD)) {
             setMoveShortcut(METAL_CLAW, MOVES[METAL_CLAW]);
         } else if (lastMove(METAL_CLAW)) {
             setMoveShortcut(SPIKES, MOVES[SPIKES]);
@@ -127,7 +169,7 @@ public class SkarmoryEnemy extends AbstractPokemonMonster
             case SPIKES: {
                 Details statusDetail = new Details(this, STATUS, WOUND_TEXTURE, Details.TargetType.DISCARD_PILE);
                 details.add(statusDetail);
-                Details powerDetail = new Details(this, BUFF, STRENGTH_TEXTURE);
+                Details powerDetail = new Details(this, BUFF, THORNS_TEXTURE);
                 details.add(powerDetail);
                 break;
             }
